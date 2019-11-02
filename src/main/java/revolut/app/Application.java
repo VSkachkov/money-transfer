59170b6619d6e2fd3fa6a7a34fa19a089bc85914
skach <vitali.skachkov@gmail.com>
2019-11-02 18:28:37 +0300

    Integration with Spark

--- src/main/java/revolut/app/Application.java
+++ src/main/java/revolut/app/Application.java
@@ -4,8 +4,10 @@ import revolut.app.api.user.RegistrationHandler;
 import com.sun.net.httpserver.BasicAuthenticator;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpServer;
+import revolut.domain.account.MoneyTransferDto;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.List;
@@ -13,41 +15,16 @@ import java.util.Map;
 
 import static revolut.app.Configuration.*;
 import static revolut.app.api.ApiUtils.splitQuery;
+import static spark.Spark.get;
+import static spark.Spark.post;
 
 class Application {
 
     public static void main(String[] args) throws IOException {
-        int serverPort = 8000;
-        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
 
-        RegistrationHandler registrationHandler = new RegistrationHandler(getUserService(), getObjectMapper(),
-            getErrorHandler());
-        server.createContext("/api/users/transfer", registrationHandler::handle);
-
-        HttpContext context =server.createContext("/api/hello", (exchange -> {
-
-            if ("GET".equals(exchange.getRequestMethod())) {
-                Map<String, List<String>> params = splitQuery(exchange.getRequestURI().getRawQuery());
-                String noNameText = "Anonymous";
-                String name = params.getOrDefault("name", List.of(noNameText)).stream().findFirst().orElse(noNameText);
-                String respText = String.format("Hello %s!", name);
-                exchange.sendResponseHeaders(200, respText.getBytes().length);
-                OutputStream output = exchange.getResponseBody();
-                output.write(respText.getBytes());
-                output.flush();
-            } else {
-                exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
-            }
-            exchange.close();
-        }));
-        context.setAuthenticator(new BasicAuthenticator("myrealm") {
-            @Override
-            public boolean checkCredentials(String user, String pwd) {
-                return user.equals("admin") && pwd.equals("admin");
-            }
-        });
-
-        server.setExecutor(null); // creates a default executor
-        server.start();
+        post("/hello", (req, res) -> {
+            final String body = req.body();
+            MoneyTransferDto dto = getObjectMapper().convertValue(body, MoneyTransferDto.class);
+            return dto.toString();});
     }
 }
---
+++ src/main/java/revolut/domain/account/Balance.java
@@ -0,0 +1,8 @@
+package revolut.domain.account;
+
+import java.math.BigDecimal;
+
+public class Balance {
+    private BigDecimal balance;
+    private Currency currency;
+}
