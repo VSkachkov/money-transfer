7331b3bd40f24c8d7a26e712068f6fc35774ef9a
skach <vitali.skachkov@gmail.com>
2019-11-02 18:58:48 +0300

    Integration with GSON

--- src/main/java/revolut/app/Application.java
+++ src/main/java/revolut/app/Application.java
@@ -1,30 +1,24 @@
 package revolut.app;
 
-import revolut.app.api.user.RegistrationHandler;
-import com.sun.net.httpserver.BasicAuthenticator;
-import com.sun.net.httpserver.HttpContext;
-import com.sun.net.httpserver.HttpServer;
+import com.google.gson.Gson;
 import revolut.domain.account.MoneyTransferDto;
+import revolut.domain.account.UserAccountService;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.util.List;
-import java.util.Map;
 
-import static revolut.app.Configuration.*;
-import static revolut.app.api.ApiUtils.splitQuery;
-import static spark.Spark.get;
+import static revolut.app.Configuration.getUserService;
 import static spark.Spark.post;
 
 class Application {
-
     public static void main(String[] args) throws IOException {
-
+        Configuration configuration = new Configuration();
+        Gson gson = new Gson();
         post("/hello", (req, res) -> {
             final String body = req.body();
-            MoneyTransferDto dto = getObjectMapper().convertValue(body, MoneyTransferDto.class);
-            return dto.toString();});
+            res.header("Content-type", "Application/JSON");
+            final MoneyTransferDto dto = gson.fromJson(body, MoneyTransferDto.class);
+            getUserService().createTransaction(dto);
+            return gson.toJson(dto);
+        });
     }
 }
